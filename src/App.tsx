15b64bce1bf915c196986b4f032e718d61d7c889
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2020-06-04 21:10:46 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Flex,
+  Heading,
+  Stack,
+  Box,
+  ThemeProvider,
+  theme,
+  CSSReset,
+  Button,
+} from "@chakra-ui/core";
+
+/** returns either 0 or 1 with a 50% chance of either */
+const getRandomIndex = () => Math.floor(Math.random() * 2);
+
+const leftBoxColours = ["#f5481e", "#00a1ff"];
+const middleBoxColours = ["#000", "#fd65f7"];
+const rightBoxColours = ["#69169a", "#fdfa65"];
 
 function App() {
+  const [leftBoxColour, setLeftBoxColour] = useState<string>(leftBoxColours[0]);
+
+  const [middleBoxColour, setMiddleBoxColour] = useState<string>(
+    middleBoxColours[0]
+  );
+  const [rightBoxColour, setRightBoxColour] = useState<string>(
+    rightBoxColours[0]
+  );
+
+  const changeColours = () => {
+    setLeftBoxColour(leftBoxColours[getRandomIndex()]);
+    setMiddleBoxColour(middleBoxColours[getRandomIndex()]);
+    setRightBoxColour(rightBoxColours[getRandomIndex()]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Flex flexDir="column" alignItems="center" h="100vh" w="100vw" p="20px">
+        <Heading mb="100px" fontSize="72px">
+          The Stem Cell Game
+        </Heading>
+        <Stack isInline spacing="50px" mb="100px">
+          <Box size="300px" borderRadius="50%" bg={leftBoxColour} />
+          <Box size="300px" borderRadius="50%" bg={middleBoxColour} />
+          <Box size="300px" borderRadius="50%" bg={rightBoxColour} />
+        </Stack>
+        <Button size="lg" onClick={changeColours}>
+          Change!
+        </Button>
+      </Flex>
+    </ThemeProvider>
   );
 }
 
